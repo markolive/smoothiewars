c601560e1543d5731b21633410e56f7a11dc5776
Mark Olive <mark@mohc.net>
2023-08-23 12:48:05 +0100

    Initial skeleton application

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import '@mui/material';
+import Button from '@mui/material/Button';
+import '@emotion/react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
